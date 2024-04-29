0053c743780602ff8de8c25fffac67cc94b25276
Sergei Zuev <zuevsn86@gmail.com>
2024-04-30 01:36:57 +0300

    step 2 fix

---
+++ Makefile
@@ -0,0 +1,17 @@
+install:
+	poetry install
+
+gendiff:
+	poetry run python -m gendiff.scripts.gendiff
+
+build:
+	poetry build
+
+publish:
+	poetry publish --dry-run
+
+package-install:
+	python3 -m pip install --force-reinstall --user dist/*.whl
+
+lint:
+	poetry run flake8 gendiff
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,13 +3,15 @@ import argparse
 
 # вывод справки
 # по умолчанию есть ключ -h (--help)
-#для добавления новых прописываем что-то вроде parser.add_argument('count') - это позиционный аргумент
+# для добавления новых прописываем
+# что-то вроде parser.add_argument('count')- это позиционный аргумент
 def main():
-    parser = argparse.ArgumentParser(description = 'Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(description='Compares two configuration \
+                                     files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     parser.parse_args()
-
 
 
 if __name__ == "__main__":
