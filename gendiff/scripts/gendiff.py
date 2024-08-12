b92c1fd7c87320b9ff704757196051632902d4df
ZuevSN <zuevsn86@gmail.com>
2024-08-12 17:45:50 +0300

    fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,38 +7,44 @@ import json
 # что-то вроде parser.add_argument('count')- это позиционный аргумент
 
 
-def start():
+def parser():
     parser = argparse.ArgumentParser(description='Compares two configuration \
                                      files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    read(args.first_file, args.second_file)
+    gendiff(args.first_file, args.second_file)
 
 
-def read(path1, path2):
+def gendiff(path1, path2):
     data3 = {}
     data1 = json.load(open(path1))
     print(data1)
     data2 = json.load(open(path2))
     print(data2)
-    for key, value in data1.items():
-        if key in data2 and data2[key] == value:
-            data3[key] = value
+    all_keys = sorted(set(data1.keys() | data2.keys()))
+    for key in all_keys:
+        value_1 = data1.get(key)
+        value_2 = data2.get(key)
+        if value_1 == value_2:
+            data3[key] = value_1
+        elif value_1 is None:
+            data3['+ ' + key] = value_2
+        elif value_2 is None:
+            data3['- ' + key] = value_1
         else:
-            data3['- ' + key] = value
-    for key, value in data2.items():
-        if key in data1 and data1[key] == value:
-            data3[key] = value
-        else:
-            data3['+ ' + key] = value
-    print('------')
+            data3['- ' + key] = value_1
+            data3['+ ' + key] = value_2
     print(data3)
 
 
+def sum(a, b):
+    return a + b
+
+
 def main():
-    start()
+    parser()
 #   poetry run gendiff file1.json file2.json
 #    start()
 
