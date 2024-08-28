872b4096d5751e8e5383147b62423469505e1698
Sergei Zuev <zuevsn86@gmail.com>
2024-08-28 13:20:59 +0300

    fix

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -14,14 +14,15 @@ from gendiff.generate_diff import generate_diff
     ('file1.yml', 'file2.yml', 'json', 'result_json'),
 ])
 
-
 def test_gendiff(file1, file2, format, expected):
     def get_fixture_path(name):
         return os.path.join('tests/fixtures', name)
+
     def read_file(file_path):
         with open(file_path, 'r') as f:
             content = f.read()
             return content
+
     expected_result = read_file(get_fixture_path(expected))
     file_1 = get_fixture_path(file1)
     file_2 = get_fixture_path(file2)
